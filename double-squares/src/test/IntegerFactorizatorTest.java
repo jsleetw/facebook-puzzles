f76592a63f00ff67a7cb3d79ba2b5caf0a44907c
mkotsur <miccots@ua.fm>
2011-01-13 11:10:33 +0200

    Refactored the test a bit

--- double-squares/src/test/IntegerFactorizatorTest.java
+++ double-squares/src/test/IntegerFactorizatorTest.java
@@ -11,15 +11,15 @@ public class IntegerFactorizatorTest {
 
     @Test public void checkTrivialFactorization() {
 
-        checkFactorization(1, new int[]{}, new int[]{});
-        checkFactorization(2, new int[]{2}, new int[]{1});
-        checkFactorization(3, new int[]{3}, new int[]{1});
-        checkFactorization(4, new int[]{2}, new int[]{2});
-        checkFactorization(5, new int[]{5}, new int[]{1});
-        checkFactorization(6, new int[]{3,2}, new int[]{1,1});
-        checkFactorization(12, new int[]{3,2}, new int[]{1,2});
-        checkFactorization(42, new int[]{7,3,2}, new int[]{1,1,1});
-        checkFactorization(144, new int[]{3,2}, new int[]{2,4});
+        checkFactorization(1, toArray(), toArray());
+        checkFactorization(2, toArray(2), toArray(1));
+        checkFactorization(3, toArray(3), toArray(1));
+        checkFactorization(4, toArray(2), toArray(2));
+        checkFactorization(5, toArray(5), toArray(1));
+        checkFactorization(6, toArray(3,2), toArray(1,1));
+        checkFactorization(12, toArray(3,2), toArray(1,2));
+        checkFactorization(42, toArray(7,3,2), toArray(1,1,1));
+        checkFactorization(144, toArray(3,2), toArray(2,4));
     }
 
     // Helper
@@ -32,5 +32,9 @@ public class IntegerFactorizatorTest {
         for (int i=0; i<factors.length; i++) {
             assertEquals(powers[i], (int)result.get(factors[i]));
         }
+    }
+
+    private static int[] toArray(int... ints) {
+        return ints;
     }
 }
